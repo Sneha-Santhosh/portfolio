ddeb1aad1b8dbb88d71e07e2ef9585178dabc006
SS <45783979+Sneha-Santhosh@users.noreply.github.com>
2020-05-28 21:04:53 -0400

    Updates

---
+++ precache-manifest.e21474b547b9211372506540ae3729f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf4eaab74c7190893289097e8a8dea2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f66f9a993e28c9684b4b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2b2612eb8c99e1735998",
+    "url": "/static/js/2.e5758b91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5758b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66f9a993e28c9684b4b",
+    "url": "/static/js/main.53ec69dd.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
